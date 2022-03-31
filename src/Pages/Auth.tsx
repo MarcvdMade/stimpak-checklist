2454e9f6fbc3cb9fe7b03e1dda6e0c2b47a00c02
Daniel <danielabdi12@gmail.com>
2022-03-31 12:05:19 +0200

    Edited path to supabase client

--- src/Pages/Auth.tsx
+++ src/Pages/Auth.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from 'react'
-import { supabase } from './supabaseClient'
+import { supabase } from '../supabaseClient'
 import {Session} from "@supabase/supabase-js";
 
 export default function Auth() {
